e7ebf55bc421f6cd3b68c90bb9e4b1d5f7885b25
utengr1994@gmail.com <utengr1994@gmail.com>
2014-08-19 17:21:39 -0500

    Updated ui and server to work with intro.html

--- server.r
+++ server.r
@@ -5,7 +5,9 @@ library(scales)
 library(psych)
 library(caret)
 library(markdown)
+library(e1071)
 
+# Load, lean data, and creat data.frames
 wbcd <- read.csv("wisc_bc_data.csv", stringsAsFactors = FALSE)
 wbcd <- wbcd[-1]
 Diagnosis <- wbcd[,1]
@@ -16,20 +18,7 @@ normalize <- function(x) {return ((x - min(x)) / (max(x) - min(x)))}
 wbcd_raw <- as.data.frame(wbcd[-1])
 wbcd_n <- as.data.frame(lapply(wbcd[2:31], normalize))
 wbcd_z <- as.data.frame(scale(wbcd[-1]))
-
-intro1 <- "This Shiny application is a demonstration of the k Nearest Neighbor (kNN) classification algorithm.  kNN is a 'lazy' learner such that it simply takes the descriptive variables of an item, find the nearest k neighbors, where k is the number of neighbors, and classifies the item the same as the majority of the selected neighbors. The challenge for the practitioner is how to define 'distance'. With n descriptive variables, the curse of dimensionality becomes more of a problem as n increase. In two dimensions, the distance can simply be calculated as the square root of the sum of squared differences between the dimensions. But as n increases, this distance can grow to such a point that all points are distant from the item."
-
-intro2 <- "Additionally, one must be careful that the units of a variable do not overpower the distance calculation.  One way to resolve this is to scale the variables so they are all between 0 and 1 or to convert them to a normalized distribution by subtracting the mean of the variable in the entire set from each point and dividing the difference by the standard deviation of the variable in the entire set."
-
-intro3 <- "The Data tab displays the raw data used for the application.  It data is a sample of the Breast Cancer Wisconsin (Diagnostic) Data Set, located on the UCI Machine Learning Repository, found at https://archive.ics.uci.edu/ml/datasets/Breast+Cancer+Wisconsin+%28Diagnostic%29. The app allows you to scale the data by normalization (0-1) or by Z-score by using the radio buttons on the left."
-
-intro4 <- "The Stats tab calculates 6 point descriptive statistics of each variable used in algorithm. The statistics will change as you change the scaling function."
-
-intro5 <- "The Graphs tab displays relationships between the various variables.  You can select the choice and number of variables to compare in the drop down menu to the left. Highlighted variables are used in the graphs.  Hold down the CTRL key to select/un-select variables. The variables are shown in scatterplots, histograms, density plots, and rug plots.  Additionally, the correlation coefficient is calculated for each pair and shown in the upper diagonal. Note that there must be more than one (1) variable selected for the graph to function."
-
-intro6 <- "The Confusion Matrix tab displays how well the algorithm is working.  A subset of 100 observations was held out of the dataset, and the algorithm is used to predict if the sample is Benign or Malignant.  Various measure of the 'goodness' of the model are also included."
-
-intro7 <- "You can change the number of neighbors used in the prediction by moving the slider. Also note the change of predictions at certain k values when you change the scaling technique."
+colnames = names(wbcd_raw)
 
 shinyServer(function(input, output) {
    #Start Breaking up data
@@ -49,13 +38,7 @@ shinyServer(function(input, output) {
          return(info)
      })
 
-    output$intro1 <- renderPrint({print(intro1)}) 
-    output$intro2 <- renderPrint({print(intro2)})
-    output$intro3 <- renderPrint({print(intro3)})
-    output$intro4 <- renderPrint({print(intro4)}) 
-    output$intro5 <- renderPrint({print(intro5)})
-    output$intro6 <- renderPrint({print(intro6)})
-    output$intro7 <- renderPrint({print(intro7)})
+    output$intro <- renderPrint({print(intro)})           
      
    #Create summary of data in a Data Table
      output$filetable <- renderDataTable({
@@ -71,30 +54,6 @@ shinyServer(function(input, output) {
      output$confusionmatrix <- renderPrint({
      confusionMatrix(Data()$test, Data()$pred)
      })
-
-  # Pick Scaling Option for Data
-      output$choose_scale <- renderUI(function() {
-        # Pick how to scale or no scale on data
-        radioButtons("scale", "Choose Scale for Data",
-                     c("No Scale" = "noscale", "Normalization" = "normal",
-                       "Z-Score" = "zscore"), selected = "No Scale")
-      })
-
-  # Pick feature with dropdown
-      output$choose_graph <- renderUI(function() {
-        # Pick the features to plot in Pair Graphs
-        selectInput("features", "Features to Pair Graph",
-                    choices = Data()$colnames,
-                    selected = c(Data()$colnames[1],Data()$colnames[2], Data()$colnames[3]),
-                    multiple = TRUE)
-      })
-
-  # Pick knn value with Slider
-      output$choose_knn <- renderUI(function() {
-        # Pick the number of neighbors
-        sliderInput("knnval", "Choose the number of Neightbors (k)",
-            min = 1, max = 25, value = 5, step = 1)
-      })
 
   # Take all the inputs and generate the appropriate plot
       output$pairgraphs <- renderPlot(function() {
--- ui.r
+++ ui.r
@@ -2,6 +2,20 @@ library(shiny)
 library(class)
 library(gmodels)
 library(markdown)
+library(e1071)
+
+# Load, lean data, and creat data.frames
+wbcd <- read.csv("wisc_bc_data.csv", stringsAsFactors = FALSE)
+wbcd <- wbcd[-1]
+Diagnosis <- wbcd[,1]
+wbcd$diagnosis <- factor(wbcd$diagnosis, levels = c("B", "M"), labels = c("Benign", "Malignant"))
+wbcd_train_labels <- wbcd[1:469, 1]
+wbcd_test_labels <- wbcd[470:569, 1]
+normalize <- function(x) {return ((x - min(x)) / (max(x) - min(x)))}
+wbcd_raw <- as.data.frame(wbcd[-1])
+wbcd_n <- as.data.frame(lapply(wbcd[2:31], normalize))
+wbcd_z <- as.data.frame(scale(wbcd[-1]))
+colnames = names(wbcd_raw)
 
 shinyUI(pageWithSidebar(
   headerPanel("k Nearest Neighbors Example"),
@@ -13,16 +27,29 @@ shinyUI(pageWithSidebar(
     helpText(""),
     helpText(""),
     helpText("Pick how to Scale the Data for Distance from Neighbor"),
-    uiOutput("choose_scale"),
+    # Pick Scaling Option for Data
+    # Pick how to scale or no scale on data
+    radioButtons("scale", "Choose Scale for Data",
+                   c("No Scale" = "noscale", "Normalization" = "normal",
+                     "Z-Score" = "zscore"), selected = "No Scale"),
     helpText(""),
     helpText(""),
     helpText("Use CTRL-click to select multiple features"),
-    uiOutput("choose_graph"),
+    # Pick feature with dropdown
+    # Pick the features to plot in Pair Graphs
+    selectInput("features", "Features to Pair Graph",
+                choices = colnames,
+                selected = c(colnames[1],colnames[2],colnames[3]),
+                multiple = TRUE),
+    
     helpText(""),
     helpText(""),
     helpText(""),
     helpText("Pick the Number of Neighbors to Classify a 'Patient'"),
-    uiOutput("choose_knn"),
+    # Pick knn value with Slider
+    # Pick the number of neighbors
+    sliderInput("knnval", "Choose the number of Neightbors (k)",
+                min = 1, max = 25, value = 5, step = 1),
     helpText(""),
     helpText(""),
     helpText("The Confusion Matrix shows Several Error Calculations for the Model")
@@ -31,10 +58,7 @@ shinyUI(pageWithSidebar(
   mainPanel(
     h3(textOutput('caption')),
     tabsetPanel(
-      # Not how I wanted to do this, but renderHTML() would crash the rest of the app
-      tabPanel("Intro", textOutput("intro1"), textOutput("intro2"), textOutput("intro3"),
-               textOutput("intro4"), textOutput("intro5"), textOutput("intro6"),
-               textOutput("intro7")),
+      tabPanel("Intro", includeHTML("intro.html")),
       tabPanel("Data", dataTableOutput("filetable")),
       tabPanel("Stats", tableOutput("stattable")),
       tabPanel("Graphs", plotOutput("pairgraphs", height = 600)),
